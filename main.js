09d17d8c6f3b784918f8f33e7759c6762b72f361
Jonathan Geller <jonathangeller@comcast.net>
2018-05-20 13:09:58 -0400

    123

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 function fillContent(){
     var open = document.getElementById("whiteboard");
-    open.style.backgroundImage = "url(https://static1.squarespace.com/static/51423a22e4b038607dd13320/t/597787cb2994caf47901fd35/1501006682680/winnebago-logo.png)";
 }
